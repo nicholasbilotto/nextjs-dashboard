f1803e16a16325e6e203a35ebdc420857226f28b
nicholasbilotto <96328916+nicholasbilotto@users.noreply.github.com>
2024-03-20 22:18:54 -0700

    login

--- auth.config.ts
+++ auth.config.ts
@@ -12,6 +12,8 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      console.log('Deployed URL:', process.env.NEXT_PUBLIC_BASE_URL);
+      console.log('Next URL:', nextUrl);
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
@@ -22,3 +24,4 @@ export const authConfig = {
     },
   },
 } satisfies NextAuthConfig;
+3;
