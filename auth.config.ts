042241bc657feadec63faa3df2475418a7df1c7a
nicholasbilotto <96328916+nicholasbilotto@users.noreply.github.com>
2024-03-20 22:33:11 -0700

    login

--- auth.config.ts
+++ auth.config.ts
@@ -4,16 +4,10 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
-  providers: [
-    // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
-    // while this file is also used in non-Node.js environments
-  ],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      console.log('Deployed URL:', process.env.NEXT_PUBLIC_BASE_URL);
-      console.log('Next URL:', nextUrl);
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
@@ -23,5 +17,5 @@ export const authConfig = {
       return true;
     },
   },
+  providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
-3;
