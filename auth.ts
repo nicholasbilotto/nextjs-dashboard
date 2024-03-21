89dd498a1d9935f5d09b4a4271943975f092fafe
nicholasbilotto <96328916+nicholasbilotto@users.noreply.github.com>
2024-03-20 22:48:19 -0700

    login

--- auth.ts
+++ auth.ts
@@ -1,10 +1,10 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import bcrypt from 'bcrypt';
+import { authConfig } from './auth.config';
 import { sql } from '@vercel/postgres';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
-import { authConfig } from './auth.config';
+import bcrypt from 'bcrypt';
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
@@ -27,11 +27,10 @@ export const { auth, signIn, signOut } = NextAuth({
 
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
-
           const user = await getUser(email);
           if (!user) return null;
-
           const passwordsMatch = await bcrypt.compare(password, user.password);
+
           if (passwordsMatch) return user;
         }
 
